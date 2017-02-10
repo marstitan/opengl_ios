7fa145ba084371f24bf98a399a2c8f64828d7ca7
wfl <154863017@qq.com>
2017-02-10 14:55:03 +0800

    first commit

---
+++ opengl_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  opengl_ios
+//
+//  Created by Mac on 2/10/17.
+//  Copyright © 2017 dami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ opengl_ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  opengl_ios
+//
+//  Created by Mac on 2/10/17.
+//  Copyright © 2017 dami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
